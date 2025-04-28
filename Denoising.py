42a0176c30cf9a660b0ae430e4ada61264bea066
7 <52979941+lokijkethan@users.noreply.github.com>
2025-04-28 14:07:35 -0400

    parts 3 and 4

--- Denoising.py
+++ Denoising.py
@@ -41,7 +41,6 @@ for sigma in sigmas:
         for j in range(0, img.shape[1]):
             numerator = 0.0
             denominator = 0.0
-
             for w1 in range(-12, 13):
                 for w2 in range(-12, 13):
                     if not(w1 + i < 0 or w1 + i >= img.shape[0] or w2 + j < 0 or w2 + j >= img.shape[1]):
@@ -57,6 +56,50 @@ for sigma in sigmas:
     plt.imshow(denoised, cmap='gray')
     plt.show()
 
+
+
+    # part 3
+
+def bilateral_weight(x1, y1, x2, y2, sigma_s, sigma_i):
+    spatial = gaussian_weighting(x1,y1,x2,y2, sigma_s)
+    intensity = np.exp(-((img_noise[x1, y1] - img_noise[x2, y2]) ** 2) / (2 * sigma_i ** 2))
+    return spatial * intensity
+
+window_radius = 12     
+spacial_sigma = [1, 5, 25]
+intensity_sigma = [1, 5, 25, 50, 500]
+
+for sigma_s in spacial_sigma:
+    for sigma_i in intensity_sigma:
+        denoised_bilat = np.zeros_like(img_noise, dtype=float)
+        for i in range(img.shape[0]):
+            for j in range(img.shape[1]):
+                numerator = 0.0
+                denominator = 0.0
+                for w1 in range(-12,13):
+                    for w2 in range(-12,13):
+                        if 0 <= i + w1 < img.shape[0] and 0 <= j + w2 < img.shape[1]:
+                            weight = bilateral_weight(i, j, i + w1, j + w2, sigma_s, sigma_i)
+                            numerator += img_noise[i + w1, j + w2] * weight
+                            denominator += weight
+                denoised_bilat[i, j] = numerator / denominator
+
+        print(f"σ_s={sigma_s:>2}, σ_i={sigma_i:>3}  PSNR: {psnr(img, denoised_bilat):.2f} dB")
+        plt.imshow(denoised_bilat, cmap='gray')
+        plt.title(f"σs={sigma_s}, σi={sigma_i}")
+        plt.axis('off')
+        plt.show()
+
+# part 4
+
+for h_val in [1, 5, 25, 50, 500]:
+        den_uint8 = cv2.fastNlMeansDenoising(img_noise.astype(np.uint8),h=h_val,templateWindowSize=7,searchWindowSize=25)
+        den = den_uint8.astype(np.float32)
+        plt.imshow(den, cmap="gray")
+        plt.show()
+        print(f"NLM h={h_val:>3}: {psnr(img, den):.2f} dB")
+
+
 # part 5
 sigmas = [5 / 255, 20 / 255, 50 / 255]
 img_noise_scale = img_noise / 255
@@ -68,13 +111,3 @@ for sigma in sigmas:
 
     plt.imshow(denoised_bm3d, cmap='gray')
     plt.show()
-
-
-
-
-
-
-
-
-
-
